6f26f49cfdd975e702735c1cc734f2086783c0f6
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-07-31 12:11:46 +0400

    removed unnecessary generators (converting to posthoc contributions)

--- datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from .metadata import GenotypeMetadata
-from .genotype_dataset import GenotypeDataset, build_dataset
--- generators/classical/__init__.py
+++
@@ -1,4 +0,0 @@
-from generators.classical.independent_snp import IndependentSNPGenerator
-from generators.classical.beta_freq_hwe import BetaFrequencyHWEGenerator
-from generators.classical.balding_nichols import BaldingNicholsGenerator
-from generators.classical.latent_gauss_copula import LatentGaussianCopulaGenerator
