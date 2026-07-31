64882f66c1762e40cc77a6a7c1fe1fb04a78fc20
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-07-31 21:08:03 +0400

    [fix](qc) uploaded original plink glm file for comparative purposes

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -4,7 +4,7 @@ PLINK_JOIN_COLS = ["#CHROM", "POS", "ID", "REF", "ALT"]
 
 
 def test_plink_output_is_joinable_with_posthoc_schema():
-    plink_df = pd.read_csv("plink_ref.PHENO1.glm.logistic.hybrid", sep=r"\s+")
+    plink_df = pd.read_csv("tests/plink_ref.PHENO1.glm.logistic.hybrid", sep=r"\s+")
 
     # posthoc's writer must share these columns so results can be merged
     for col in PLINK_JOIN_COLS:
