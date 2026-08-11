e6c6da17abb743b932e0a924c9e59d711b945b5f
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-08-11 14:47:12 +0400

    [feat](attr) Added pal computation to main commands

--- posthoc/cli.py
+++ posthoc/cli.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import click
 
+from posthoc.commands.pal import pal
 from posthoc.commands.baseline import baseline
 from posthoc.commands.run import run
 from posthoc.commands.simulate_pheno import simulate_pheno
@@ -17,6 +18,7 @@ def main(verbose: bool) -> None:
 main.add_command(run)
 main.add_command(baseline)
 main.add_command(simulate_pheno)
+main.add_command(pal)
 
 if __name__ == "__main__":
     main()
