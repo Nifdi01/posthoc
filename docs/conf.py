a51241b188054c468f6a713367850784c1f2c961
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2026-08-13 10:44:04 +0400

    [feat](docs) Add project metadata and configuration to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,16 @@
+from __future__ import annotations
 
+project = "PostHoc"
+author = "Nifdi Guliyev"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
